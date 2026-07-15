fb390ad53a895ed922670698a555ff9553785fb2
Anushka Jadhav <Anushka@ho.adiance.local>
2026-07-15 17:38:50 +0530

    mail logs done

--- MERN-ECommerce-Project/middleware/mail.js
+++ MERN-ECommerce-Project/middleware/mail.js
@@ -1,23 +1,29 @@
-// config/mail.js
-
 const nodemailer = require("nodemailer");
+console.log("reading mail.js file");
+
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMPT_MAIL,
     pass: process.env.SMPT_PASSWORD,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
+console.log("MAIL:", process.env.SMPT_MAIL);
+console.log("PASSWORD EXISTS:", !!process.env.SMPT_PASSWORD);
 
-// Verify SMTP connection when server starts
 (async () => {
   try {
     await transporter.verify();
     console.log("✅ SMTP Server Connected Successfully");
   } catch (error) {
     console.error("❌ SMTP Connection Failed");
-    console.error(error.message);
+    console.error(error);
   }
 })();
 
